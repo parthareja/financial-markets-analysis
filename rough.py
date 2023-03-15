6556116411987d00a2eeb8552d556fbc4540507b
Parth Thareja <parth.thareja.pt@gmail.com>
2023-03-15 19:16:15 +0530

    test change

---
+++ rough.py
@@ -0,0 +1,9 @@
+
+# import yfinance as yf
+# import pandas as pd
+
+# data = yf.download('HDFCBANK.NS', period = '1mo', interval = '1d', threads = True, group_by = 'ticker', progress = False)
+
+
+# a = 'https://www1.nseindia.com/content/indices/ind_niftymidcapselect_list.csv'
+# print(pd.read_csv(a))
