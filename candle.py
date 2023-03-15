96ffaea00cabca3b5d04e697c0601c7e4f266f31
Parth Thareja <parth.thareja.pt@gmail.com>
2023-03-15 20:36:00 +0530

    candle class complete

--- candle.py
+++ candle.py
@@ -3,15 +3,11 @@ class Candle:
         self.data = dataframe
         self.ticker = ticker
         
-
-
     def open(self, index):
         return self.data.iat[index - 1, self.data.columns.get_loc((self.ticker, 'Open'))]
 
     def close(self, index):
         return self.data.iat[index - 1, self.data.columns.get_loc((self.ticker, 'Close'))]
-        # return self.data.iat[index - 1, self.data.columns.get_loc((self.ticker, 'Adj Close'))]
-        # pass
 
     def high(self, index):
         return self.data.iat[index - 1, self.data.columns.get_loc((self.ticker, 'High'))]
@@ -20,29 +16,10 @@ class Candle:
         return self.data.iat[index - 1, self.data.columns.get_loc((self.ticker, 'Low'))]
 
     def colour(self, index):
-        if self.close() > self.open():
-            colour = 'Green'
-        elif self.close() < self.open():
-            colour = 'Red'
-        # else:
-        #     colour = 
+        if self.close(index) > self.open(index):
+            colour = 'GREEN'
+        elif self.close(index) < self.open(index):
+            colour = 'RED'
+        else:
+            colour = 'NA'
         return colour
-
-
-
-# symbolsList = ['HDFCBANK.NS', 'TCS.NS', 'ADANIPORTS.NS', 'APOLLOHOSP.NS', 'ASIANPAINT.NS', 'AXISBANK.NS']
-# # symbolsList = ['HDFCBANK.NS', 'TCS.NS']
-
-
-# ####### DRIVER
-# from data import Data
-# a = Data(symbolsList, '1mo')
-
-# data = a.fetchData()
-# print(data)
-
-# b = Candle(data, 'ADANIPORTS.NS')
-# print(b.high(-6))
-
-
-# index as object (class) attribute or method attrobute?????
